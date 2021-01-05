cdadae68ccede3377d92f24d0611fe0a245099c0
user <manjunathan.m@contus.in>
2021-01-05 19:54:06 +0530

    Resume updated

--- scripts/main.js
+++ scripts/main.js
@@ -41,3 +41,25 @@ $('a.smooth-scroll')
     }
   }
 });
+
+$(document).ready(function(){
+	
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) 
+		{
+			$('.scroll_To_Top').fadeIn();
+		} 
+		else 
+		{
+			$('.scroll_To_Top').fadeOut();
+		}
+	});
+	
+
+	$('.scroll_To_Top').click(function(){
+		$('html, body').animate({scrollTop : 0},700);
+		return false;
+	});
+	
+});
+
